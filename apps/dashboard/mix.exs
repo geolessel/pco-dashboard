7c8e8610b372e3c6cfc400bcd91d1a923abff4df
Geoffrey Lessel <geo@ministrycentered.com>
2020-04-28 09:01:18 -0700

    use pco production connection even in dev
    There's no reason not to use the production public endpoint for API
    requests. Since this is a read-only application and no internal
    secrets are required for connection, let's connect to real, live data
    from the onset.

--- apps/dashboard/mix.exs
+++ apps/dashboard/mix.exs
@@ -41,7 +41,8 @@ defmodule Dashboard.MixProject do
       {:ecto_sql, "~> 3.4"},
       {:postgrex, ">= 0.0.0"},
       {:jason, "~> 1.0"},
-      {:mint, "~> 1.0"}
+      {:mint, "~> 1.0"},
+      {:castore, "~> 0.1.5"}
     ]
   end
 
