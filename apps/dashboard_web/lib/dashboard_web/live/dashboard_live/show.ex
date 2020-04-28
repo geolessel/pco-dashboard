8f3b2d7de7303912258f4ebf7cfffb8a4825a81d
Geoffrey Lessel <geo@ministrycentered.com>
2020-04-28 13:53:05 -0700

    programmatically build dashboard page from list of components
    right now, just from a hard-coded List of a single Map, but this paves
    the way for database-backed components!

--- apps/dashboard_web/lib/dashboard_web/live/dashboard_live/show.ex
+++ apps/dashboard_web/lib/dashboard_web/live/dashboard_live/show.ex
@@ -10,6 +10,7 @@ defmodule DashboardWeb.DashboardLive.Show do
 
     {:ok,
      socket
+     |> assign(:components, [])
      |> assign(:user_token, user_token)}
   end
 
@@ -18,9 +19,18 @@ defmodule DashboardWeb.DashboardLive.Show do
     user = Accounts.get_user_by_session_token(socket.assigns.user_token)
     dashboard = Dashboards.get_dashboard_by_slug!(slug, user.id)
 
+    components = [
+      %{
+        assign: :people,
+        module: "DashboardWeb.Components.PersonUpdated",
+        api_path: "/people/v2/people?order=-updated_at&per_page=5&fields[Person]=name,updated_at"
+      }
+    ]
+
     {:noreply,
      socket
      |> assign(:page_title, page_title(socket.assigns.live_action))
+     |> assign(:components, components)
      |> assign(:people, [])
      |> assign(:dashboard, dashboard)}
   end
@@ -29,23 +39,21 @@ defmodule DashboardWeb.DashboardLive.Show do
   def handle_info(:fetch_people, socket) do
     user = Accounts.get_user_by_session_token(socket.assigns.user_token)
 
-    people =
-      Map.get(
-        Dashboard.PlanningCenterApi.Client.get(
-          user,
-          "/people/v2/people?order=-updated_at&per_page=5&fields[Person]=name,updated_at"
-        ),
-        :body,
-        %{}
-      )
-      |> Map.get("data", [])
+    socket =
+      Enum.reduce(socket.assigns.components, socket, fn component, acc ->
+        assign(
+          socket,
+          component.assign,
+          Map.get(
+            Dashboard.PlanningCenterApi.Client.get(user, component.api_path),
+            :body,
+            %{}
+          )
+          |> Map.get("data", [])
+        )
+      end)
 
-    {:noreply,
-     assign(
-       socket,
-       :people,
-       people
-     )}
+    {:noreply, socket}
   end
 
   defp page_title(:show), do: "Show Dashboard"
