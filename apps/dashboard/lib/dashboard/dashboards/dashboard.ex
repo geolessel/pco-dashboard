1e7c239ed35651b37bc55962d9e0d95ddd71ff02
Geoffrey Lessel <geo@ministrycentered.com>
2020-04-28 11:25:59 -0700

    restrict characters acceptable in a slug

--- apps/dashboard/lib/dashboard/dashboards/dashboard.ex
+++ apps/dashboard/lib/dashboard/dashboards/dashboard.ex
@@ -19,6 +19,9 @@ defmodule Dashboard.Dashboards.Dashboard do
     |> validate_required([:name, :slug, :user_id])
     |> validate_length(:name, min: 3)
     |> validate_length(:slug, min: 3)
+    |> validate_format(:slug, ~r/^[a-zA-Z0-9-_]+$/,
+      message: "must contain only letters, numbers, dashes (-), or underscores (_)"
+    )
     |> assoc_constraint(:user)
     |> unique_constraint([:slug, :user_id])
   end
