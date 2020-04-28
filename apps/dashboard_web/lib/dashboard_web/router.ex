f3889ffc086488ce9a37b6d2cf00156133e51812
Geoffrey Lessel <geo@ministrycentered.com>
2020-04-28 09:12:08 -0700

    Put the Phoenix live dashboard scoped to /admin

--- apps/dashboard_web/lib/dashboard_web/router.ex
+++ apps/dashboard_web/lib/dashboard_web/router.ex
@@ -38,7 +38,7 @@ defmodule DashboardWeb.Router do
   if Mix.env() in [:dev, :test] do
     import Phoenix.LiveDashboard.Router
 
-    scope "/" do
+    scope "/admin" do
       pipe_through :browser
       live_dashboard "/dashboard", metrics: DashboardWeb.Telemetry
     end
