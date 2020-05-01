983cbb1704150499b76c29e2ddeeb27ceab86a98
Jesse J. Anderson <jessejanderson@gmail.com>
2020-05-01 11:53:52 -0700

    style: Edit Dashboard modal

--- apps/dashboard_web/lib/dashboard_web/live/modal_component.ex
+++ apps/dashboard_web/lib/dashboard_web/live/modal_component.ex
@@ -11,7 +11,7 @@ defmodule DashboardWeb.ModalComponent do
       phx-target="#<%= @id %>"
       phx-page-loading>
 
-      <div class="phx-modal-content">
+      <div class="phx-modal-content max-w-sm">
         <%= live_patch raw("&times;"), to: @return_to, class: "phx-modal-close" %>
         <%= live_component @socket, @component, @opts %>
       </div>
