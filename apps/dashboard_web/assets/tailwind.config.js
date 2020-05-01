9b0308dda1f04f7ac0b006a18951139029f77b54
Jesse J. Anderson <jessejanderson@gmail.com>
2020-04-30 17:00:55 -0700

    feat: add product colors to tailwind

--- apps/dashboard_web/assets/tailwind.config.js
+++ apps/dashboard_web/assets/tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        app: {
+          calendar: "#cc4832",
+          "check-ins": "#876096",
+          giving: "#f2b327",
+          groups: "#fb7642",
+          people: "#5282e5",
+          registrations: "#46948d",
+          services: "#659630"
+        }
+      }
+    }
   },
   variants: {},
   plugins: []
