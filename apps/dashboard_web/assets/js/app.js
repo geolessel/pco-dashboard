d01cf9c88ec5288c7c3aa9314fb93c3b971ad3f6
Jesse J. Anderson <jessejanderson@gmail.com>
2020-05-05 11:35:56 -0700

    feat: WIP add c3 chart to chard component

--- apps/dashboard_web/assets/js/app.js
+++ apps/dashboard_web/assets/js/app.js
@@ -17,6 +17,18 @@ import { Socket } from "phoenix"
 import NProgress from "nprogress"
 import { LiveSocket } from "phoenix_live_view"
 
+import * as d3 from "d3"
+import c3 from "c3"
+  var chart = c3.generate({
+    bindto: '#chart',
+    data: {
+      columns: [
+        ['data1', 30, 200, 100, 400, 150, 250],
+        ['data2', 50, 20, 10, 40, 15, 25]
+      ]
+    }
+  });
+
 import ComponentDragAndDrop from "./dragAndDropComponents"
 let Hooks = { ComponentDragAndDrop }
 
