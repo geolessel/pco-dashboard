f07cc1e8c4cfa88e2169d184e489cffb1f499c75
Jesse J. Anderson <jessejanderson@gmail.com>
2020-05-04 20:59:44 -0700

    catch nil results

--- apps/dashboard_web/lib/dashboard_web/live/component_helpers.ex
+++ apps/dashboard_web/lib/dashboard_web/live/component_helpers.ex
@@ -13,7 +13,7 @@ defmodule DashboardWeb.ComponentHelpers do
 
   def get_trend_percent(cur, prev), do: get_percent(cur, prev) |> Float.round(1) |> add_plus_str()
 
-  def get_human_number(n) do
+  def get_human_number(n) when is_number(n) do
     case n do
       n when n >= 100_000_000 -> "#{round(n / 1_000_000)}M"
       n when n >= 10_000_000 -> "#{Float.round(n / 1_000_000, 1)}M"
@@ -25,9 +25,13 @@ defmodule DashboardWeb.ComponentHelpers do
     end
   end
 
+  def get_human_number(_), do: "0"
+
   defp add_plus_str(num) when num > 0, do: "+#{num}"
   defp add_plus_str(num), do: "#{num}"
 
+  defp get_percent(nil, _), do: 0.0
+  defp get_percent(_, nil), do: 0.0
   defp get_percent(0, 0), do: 0.0
   defp get_percent(_, 0), do: -100.0
   defp get_percent(0, _), do: 100.0
