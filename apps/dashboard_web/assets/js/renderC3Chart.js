31f10885e217cc6334215abefabe506a41ec43d0
Jesse J. Anderson <jessejanderson@gmail.com>
2020-05-06 16:39:05 -0700

    style: c3 styles

--- apps/dashboard_web/assets/js/renderC3Chart.js
+++ apps/dashboard_web/assets/js/renderC3Chart.js
@@ -4,8 +4,25 @@ export default {
   mounted() {
     this.chart = c3.generate({
       bindto: "#chart",
+      color: {
+        pattern: [
+          "#a0aec0", // gray-500
+          "#4299e1", // blue-500
+        ]
+      },
       data: {
         columns: JSON.parse(this.el.dataset.chartColumns),
+      },
+      point: {
+        r: 2
+      },
+      grid: {
+        x: {
+          show: true
+        },
+        y: {
+          show: true
+        },
       },
     })
 
