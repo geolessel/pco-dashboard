c81b7f9d15b521257b9999f34f6832d40154a19f
Geoffrey Lessel <geo@ministrycentered.com>
2020-05-07 15:05:31 -0700

    feat: dynamically set the ListResult title

--- apps/dashboard/lib/dashboard/components/list_results.ex
+++ apps/dashboard/lib/dashboard/components/list_results.ex
@@ -5,15 +5,25 @@ defmodule Dashboard.Components.ListResults do
   @impl true
   def data_sources do
     %{
-      results: "/people/v2/lists/${list_id}/people?per_page=8&order=-created_at"
+      results: "/people/v2/lists/${list_id}/people?per_page=8&order=-created_at",
+      list: "/people/v2/lists/${list_id}&fields[List]=name,description"
     }
   end
 
   @impl true
-  def process_data(%{results: %Response{} = response} = state) do
+  def process_data(%{results: %Response{} = response, list: %Response{} = list_response} = state) do
     results = Response.dig(response, [:body, "data"])
+
+    list_attributes = Response.dig(list_response, [:body, "data", "attributes"])
+
+    list_name =
+      case list_attributes["name"] do
+        nil -> list_attributes["description"]
+        name -> name
+      end
 
     state
     |> Map.put(:results, results)
+    |> Map.put(:list_name, list_name)
   end
 end
