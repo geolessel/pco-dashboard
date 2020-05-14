233e1d8a9c99db2d25a99647f9dabb04fd88a594
Geoffrey Lessel <geo@ministrycentered.com>
2020-05-13 17:48:49 -0700

    change(Release): add seed/1 function to seed database
    Run with
    
    `docker-compose exec web bin/dashboard eval "Dashboard.Release.seed"`

--- apps/dashboard/lib/release.ex
+++ apps/dashboard/lib/release.ex
@@ -1,10 +1,32 @@
 defmodule Dashboard.Release do
   @app :dashboard
+  @start_apps [
+    :crypto,
+    :ssl,
+    :postgrex,
+    :ecto
+  ]
+
+  def seed do
+    load_app()
+
+    Enum.each(@start_apps, &Application.ensure_all_started/1)
+
+    Dashboard.Repo.start_link(pool_size: 1)
+
+    path = Path.join(["#{:code.priv_dir(@app)}", "repo", "seeds.exs"])
+
+    if File.exists?(path) do
+      IO.puts("Running seed script at #{path} ...")
+      Code.eval_file(path)
+    end
+  end
 
   def migrate do
     load_app()
 
     for repo <- repos() do
+      IO.puts("migrating repo: #{Kernel.inspect(repo)}")
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
   end
